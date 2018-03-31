0420ee1ba00188f4ac4ba864beb6bf8742eef000
Steven Shan <steven.shan99@gmail.com>
2018-03-31 17:37:40 -0400

    modified get organization data to handle requests without specific param or id

--- map_frontend/scripts/get_org_data.php
+++ map_frontend/scripts/get_org_data.php
@@ -1,58 +1,83 @@
 <?php
 
+	header("Access-Control-Allow-Origin: *");
+
 	// print data object of currently logged in user
 	// prints "-1" if not logged in
 
-	include("session.php");
-
-	if (!$success || !isset($_GET["param"]) || $_GET["param"] == "")
+	$flag = true;
+	if (!isset($_GET["id"]) || $_GET["id"] == "")
 	{
-		echo "-1";
-	}
-	else if (!isset($_GET["id"]) || $_GET["id"] == "")
-	{
-		echo "-1";
-	}
-	else
-	{
-		include("config.php");
-
-		$myid = $_SESSION["login_user"];
-		$param = mysqli_real_escape_string($db, $_GET["param"]);
-		$flag=false;
-		if ($param == "users")
+		include("organization_session.php");
+		if (!$success)
 		{
-			$param = "owner, users";
-			$flag=true;
-		}
-		$id = mysqli_real_escape_string($db, $_GET["id"]);
-
-		$sql = "SELECT $param FROM Organizations WHERE id='$id'";
-		$result=mysqli_query($db, $sql);
-		$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-
-		$count=mysqli_num_rows($result);
-
-		if($count != 1)
-		{
+			$flag = false;
 			echo "-1";
 		}
 		else
 		{
-			if ($flag)
+			$id=$_SESSION["org_user"];
+		}
+	}
+	else
+	{
+		$id=$_GET["id"];
+	}
+
+	if ($flag)
+	{
+		if (!isset($_GET["param"]) || $_GET["param"] == "")
+		{
+			include("config.php");
+
+			$id = mysqli_real_escape_string($db, $id);
+
+			$sql = "SELECT * FROM Organizations WHERE id='$id'";
+			$result=mysqli_query($db, $sql);
+			$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+			echo json_encode($row);
+		}
+		else 
+		{
+			include("config.php");
+
+			$param = mysqli_real_escape_string($db, $_GET["param"]);
+			$flag=false;
+			if ($param == "users")
 			{
-				if ($row["users"] == "")
-				{
-					echo $row["owner"];
-				}
-				else
-				{
-					echo $row["owner"] . "," . $row["users"];
-				}
+				$param = "owner, users";
+				$flag=true;
+			}
+			$id = mysqli_real_escape_string($db, $id);
+
+			$sql = "SELECT $param FROM Organizations WHERE id='$id'";
+			$result=mysqli_query($db, $sql);
+			$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+			$count=mysqli_num_rows($result);
+
+			if($count != 1)
+			{
+				echo "-1";
 			}
 			else
 			{
-				echo $row[$param];
+				if ($flag)
+				{
+					if ($row["users"] == "")
+					{
+						echo $row["owner"];
+					}
+					else
+					{
+						echo $row["owner"] . "," . $row["users"];
+					}
+				}
+				else
+				{
+					echo $row[$param];
+				}
 			}
 		}
 	}
