e89f6f33b3d7a028a18f95af4369772e094c3722
Steven Shan <steven.shan99@gmail.com>
2018-03-31 06:13:12 -0400

    added script to create new organization

---
+++ map_frontend/scripts/create_org.php
@@ -0,0 +1,54 @@
+<?php
+	
+	// Script to create a new organization using post data
+	// sets $success to true if organization was successfully created
+
+	$error="";
+	$orgid=-1;
+
+	// make sure user is logged in
+	include("session.php");
+	if (!$success)
+	{
+		$error="You must be logged in to create an organization";
+	}
+	else
+	{
+		include("config.php");
+		session_start();
+		$success=false;
+		if($_SERVER["REQUEST_METHOD"] == "POST") {
+		    $orgname=mysqli_real_escape_string($db,$_POST['name']);
+		    $orgowner=$_SESSION["login_user"];
+
+			$sql="INSERT INTO `Organizations`(`name`, `owner`) " .
+				 "VALUES ('$orgname', '$orgowner')";
+		    $result=mysqli_query($db,$sql);
+
+		    if ($result === false)
+		    {
+		    	$success = false;
+		    	$error="Unable to register organization";
+		    }
+		    else
+		    {
+		    	$orgid=$db->insert_id;
+		    	$sql="SELECT organizations FROM Users WHERE id='$orgowner'";
+		    	$result=mysqli_query($db, $sql);
+		    	$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+		    	$new_data=$row["organizations"];
+		    	if ($new_data == "")
+		    	{
+		    		$new_data = $orgid;
+		    	}
+		    	else
+		    	{
+		    		$new_data = $new_data . "," . $ordid;
+		    	}
+		    	$sql = "UPDATE `Users` SET `organizations`='$new_data' WHERE id='$orgowner'";
+		    	mysqli_query($db, $sql); 
+		    	$success = true;
+		    }
+		}
+	}
+?>
