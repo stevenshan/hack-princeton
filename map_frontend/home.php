0df6b525c77f6008733e7c40112483e6c7cc0bdd
Steven Shan <steven.shan99@gmail.com>
2018-03-31 19:50:47 -0400

    put the logo up

---
+++ map_frontend/home.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/floating-labels.css" rel="stylesheet">
+    <link href="css/home.css" rel="stylesheet" type="text/css">    
+
+    <style>
+    body  {
+       background-image: url("imgs/handsOnHands.jpg");
+       -webkit-background-size: cover;
+       -moz-background-size: cover;
+       -o-background-size: cover;
+       -background-size: cover;
+    }
+    </style>
+  </head>
+
+  <body>
+    <div class="form-signin">
+      <div class="text-center mb-4">
+        <img class="mb-4" src="imgs/logo.svg" alt="" width="256" height="256">
+        <!--<h1 class="h3 mb-3 font-weight-normal">Login</h1>-->
+      </div>
+
+      <div style='text-align:center'>
+	<big><big><big><big><big><big><big><big><big><big><big><big>
+		HOME
+	</big></big></big></big></big></big></big></big></big></big></big></big>
+      </div>
+      <p> </p>
+
+      <div id="outer">
+        <div style='text-align:center'>
+          <div class="inner"><a href="login.php"><button class="btn btn-lg btn-primary btn-block"><h4>Volunteer</h4></button></a></div>
+          <div class="inner"><a href="login_organization.php"><button class="btn btn-lg btn-primary btn-block" type="submit"><h4>Organization</h4></button></a></div>
+        </div>
+      </div>
+
+ <p> </p>
+      <p class="mt-5 mb-3 text-muted text-center">&copy; 2017-2018</p>
+    </div>
+  </body>
+</html>
