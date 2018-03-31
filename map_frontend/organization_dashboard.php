d20d1da21e23cf6ce8aa0de4ce7e4d471a0e14ca
Steven Shan <steven.shan99@gmail.com>
2018-03-31 10:32:50 -0400

    added script to make event

--- map_frontend/organization_dashboard.php
+++ map_frontend/organization_dashboard.php
@@ -18,8 +18,20 @@
 <html>
 	<head>
 		<title><?php echo $row["name"]; ?> Dashboard</title>
+		<link href="css/organization-dash.css" rel="stylesheet">
 	</head>
 	<body>
-		test
+		<header>
+			<?php echo $row["name"]; ?>
+		</header>
+		<main>
+			<div class="container">
+				t 1
+			</div>
+			<div class="container">
+				<h3>Events<a href="register_event.php"><img style="vertical-align: middle; float: right" src="imgs/add.svg"></a></h3>
+				<div class="event">fsfd</div>
+			</div>
+		</main>
 	</body>
 </html>
