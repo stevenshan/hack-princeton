4f3e07b765954b9a2a6aa94057b20e3a5bbd1aad
Steven Shan <steven.shan99@gmail.com>
2018-03-31 19:46:46 -0400

    organization dashboard displaying events

--- map_frontend/js/dashboard.js
+++ map_frontend/js/dashboard.js
@@ -9,6 +9,21 @@ function httpGetAsync(theUrl, callback)
     xmlHttp.send(null);
 }
 
+function formatDate(date) {
+	var monthNames = [
+		"January", "February", "March",
+		"April", "May", "June", "July",
+		"August", "September", "October",
+		"November", "December"
+	];
+
+	var day = date.getDate();
+	var monthIndex = date.getMonth();
+	var year = date.getFullYear();
+
+	return monthNames[monthIndex] +  ' ' + day + ', ' + year;
+}
+
 $(function(){
 	httpGetAsync("scripts/get_org_data.php", function(e){
 		e = JSON.parse(e);
@@ -19,7 +34,53 @@ $(function(){
 			events = []
 		}
 		num_events = events.length
-		
+
 		$(".event_count").html("(" + num_events + ")")
+
+		events.forEach(function(e){
+			httpGetAsync("scripts/get_event_data.php?id="+e, function(data){
+				data = JSON.parse(data);
+				content = JSON.parse(data["data"]);
+
+				date = new Date(data["date"]);
+
+				if (content["start"] != "" && content["end"] != "")
+				{
+					optionalTimes = '<div class="event-desc">Starts at ' + 
+									content["start"] + ', Ends at ' + 
+									content["end"] + '</div>';
+				}
+				else if (content["start"] != "" && content["end"] == "")
+				{
+					optionalTimes = '<div class="event-desc">Starts at ' + 
+									content["start"] + '</div>';
+				}
+				else if (content["start"] == "" && content["end"] != "")
+				{
+					optionalTimes = '<div class="event-desc">Ends at ' + 
+									content["end"] + '</div>';
+				}
+				else
+				{
+					optionalTimes = "";
+				}
+
+				if (content["description"] != "")
+				{
+					optionalDesc = '<div class="event-right">' + content["description"] + '</div>';
+				}
+				else
+				{
+					optionalDesc = "";
+				}
+
+				element = '<label class="event"> \
+					<div class="event-name">' + data["name"] + '<span>' +
+					formatDate(date) + '</span></div>' + optionalTimes +  
+					optionalDesc + '</label>';
+				console.log(content);
+				$("#events-container").append(element);
+			})
+		});
 	});
 });
