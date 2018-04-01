b2fbd11874f93d5426f1a293416c278de08251ea
Matthew Feng <mattfeng@mit.edu>
2018-04-01 00:12:12 -0400

    hopefully cookies working

--- map_frontend/scripts/session.php
+++ map_frontend/scripts/session.php
@@ -5,15 +5,5 @@
 
 	session_start();
 	$success=isset($_SESSION["login_user"]);
-	setcookie("uid", $_SESSION["login_user"], time() + (86400 * 30), "/");
-
-	$sql = "SELECT name FROM Users WHERE id=" + $_SESSION["login_user"];
-	$result = mysqli_query($db, $sql);
-	$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-
-	$count = mysqli_num_rows($result);
-
-	if($count == 1)
-		setcookie("username", $row["name"], time() + (86400 * 30), "/");
 
 ?>
